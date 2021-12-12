d8f67f1dda131120579fe16d86e42a6875a6cabb
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-12-12 14:34:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+float numMAX (const float num[], int dimensione){
+
+    num [dimensione];
+    float max = num[0];
+    for(int i=0;i<dimensione ; i++){
+
+        if (max<num[i]){
+            max = num [i];
+        }
+
+    }
+   return max;
+
+}
 
