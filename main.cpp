eba72f9c4f689ab60bf627c1d728efbcfa874914
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-12-12 14:35:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+int dimensione;
+std::cin >> dimensione;
+float num [dimensione];
+for(int i=0; i<dimensione; i++){
+
+    std::cin>> num[i];
+}
+    numMAX(num,dimensione);
+
+std::cout << numMAX(num,dimensione) << std::endl;
+
+return 0;
 }
